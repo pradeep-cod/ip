8e3252c9ddb4365f0b70a6f7ee6efea3d576dfbb
pradeep-cod <110800450+pradeep-cod@users.noreply.github.com>
2024-01-24 16:02:57 +0800

    Level 0: Rename, Greet, Exit

--- ip-master/src/main/java/Duke.java
+++ ip-master/src/main/java/Duke.java
@@ -1,11 +1,22 @@
+//public class Duke {
+//    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+//    }
+//}
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________\n" +
+                "Hello! I'm Leo\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________"
+        );
     }
 }
-// hi chatbot
