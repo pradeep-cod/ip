ef1416c9c63570866cde7aca9337bf63622b7f1e
pradeep-cod <pradeepmanimaran@gmail.com>
2024-01-31 16:21:50 +0800

    Level 1: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,30 @@
 
 
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
-                "Hello! I'm Leo\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________"
-        );
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Sura");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if ("bye".equalsIgnoreCase(userInput)) {
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________");
+        }
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
