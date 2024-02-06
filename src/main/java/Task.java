8c4ecd0d6e3ce0ccc1cc07cc8882091c4b3a871e
pradeep-cod <pradeepmanimaran@gmail.com>
2024-02-06 12:55:18 +0800

    Level 4: Todo, Deadline, Events

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -18,4 +18,8 @@ public class Task {
     public void unmarkAsDone() {
         this.isDone = false;
     }
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
 }
